7652294e72b662ddac0d4545418e7b8335357de2
ginosyan00 <gurgenginosyan@neetrino.com>
2026-03-06 17:51:02 +0400

    էէէ

--- packages/database/src/generated/client/edge.js
+++ packages/database/src/generated/client/edge.js
@@ -549,6 +549,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- packages/database/src/generated/client/index.js
+++ packages/database/src/generated/client/index.js
@@ -550,6 +550,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
